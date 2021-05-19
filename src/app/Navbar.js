03e38f0dd9bce1603f53a40c6b38f46196115a00
Cyrus <wiranenar@gmail.com>
2021-05-19 15:55:41 +0300

    title changed

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className="navbar component">
-                <h1>Eclipse-studio</h1>
+                <h1>Currency tools</h1>
                 <div className="links">
                     <Link className="links__link" to="/currencies">Список валют</Link>
                     <Link className="links__link" to="/converter">Конвертер</Link>
